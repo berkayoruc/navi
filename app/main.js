494682dbbd8493440645a5edc0e03db6294c4a1d
Berkay ORUÇ <berkay.oruc@ibb.gov.tr>
2022-12-16 14:44:28 +0300

    base

--- app/main.js
+++ app/main.js
@@ -2,7 +2,7 @@ let map = new maplibregl.Map({
 	container: 'map', // container id
 	center: [28.787520307356175, 40.97431939329584], // starting position [lng, lat]
 	zoom: 17.5, // starting zoom
-	style: 'https://openmaptiles.github.io/positron-gl-style/style-cdn.json',
+	style: 'https://basemaps.cartocdn.com/gl/positron-gl-style/style.json',
 });
 
 let geolocateControl = new maplibregl.GeolocateControl({

2849bb62195ba9acb80175225fe70ba1e2270f13
Berkay ORUÇ <berkay.oruc@ibb.gov.tr>
2022-12-16 13:59:42 +0300

    first commit

---
+++ app/main.js
@@ -0,0 +1,34 @@
+let map = new maplibregl.Map({
+	container: 'map', // container id
+	center: [28.787520307356175, 40.97431939329584], // starting position [lng, lat]
+	zoom: 17.5, // starting zoom
+	style: {
+		version: 8,
+		sources: {},
+		layers: [
+			{
+				id: 'background',
+				type: 'background',
+				paint: {
+					'background-color': '#ff00ff',
+				},
+			},
+		],
+	},
+});
+
+let geolocateControl = new maplibregl.GeolocateControl({
+	positionOptions: {
+		enableHighAccuracy: true,
+	},
+	trackUserLocation: true,
+	showUserLocation: true,
+});
+map.addControl(geolocateControl, 'bottom-left');
+geolocateControl.on('geolocate', (e) => {
+	console.log('A geolocate event has occurred.');
+	console.log('lng:' + e.coords.longitude + ', lat:' + e.coords.latitude);
+	const locationInfo = document.getElementById('locationInfo');
+	locationInfo.innerHTML =
+		'lng:' + e.coords.longitude + ', lat:' + e.coords.latitude;
+});
